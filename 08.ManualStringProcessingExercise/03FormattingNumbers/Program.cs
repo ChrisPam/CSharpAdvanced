be0bd53ab2609310427bc6ffd5b8616de5284c3a
Kristiyan Pamidov <chrispam@students.softuni.bg>
2017-06-18 20:25:16 +0300

    Manual string proccessing solutions

--- 08.ManualStringProcessingExercise/03FormattingNumbers/Program.cs
+++ 08.ManualStringProcessingExercise/03FormattingNumbers/Program.cs
@@ -6,21 +6,15 @@ public class FormattingNumbers
 {
     public static void Main()
     {
-        var input = Console.ReadLine().Split(new char[] { ' ' },
+        var input = Console.ReadLine().Split(new char[] { ' ', '\t' },
             StringSplitOptions.RemoveEmptyEntries);
-        var a = int.Parse(input[0]);
+        var a = long.Parse(input[0]);
         var aToHex = a.ToString("X");
-
-        if(aToHex.Length > 10)
-        {
-            var temp = aToHex.ToList();
-            while (temp.Count > 10)
-            {
-                temp.Remove(temp.First());
-            }
-            aToHex = temp.ToString();
-        }
         var aToBinary = Convert.ToString(a, 2);
+        if (aToBinary.Length >= 10)
+        {
+            aToBinary = aToBinary.Substring(0, 10);
+        }
         var b = decimal.Parse(input[1]);
         var c = decimal.Parse(input[2]).ToString("f3");
         Console.WriteLine($"|{aToHex.PadRight(10)}|{aToBinary.PadLeft(10, '0')}|{b,10:f2}|{c.PadRight(10)}|");
---
+++ 08.ManualStringProcessingExercise/15.MelrahShake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        var text = Console.ReadLine();
+        var pattern = Console.ReadLine();
+
+        while (true)
+        {
+            var length = pattern.Count() / 2;
+            var startIndex = text.IndexOf(pattern);
+            var endIndex = text.LastIndexOf(pattern);
+            if (startIndex == -1 || startIndex == endIndex)
+            {
+                break;
+            }
+
+            text = text.Remove(endIndex, pattern.Length);
+            text = text.Remove(startIndex, pattern.Length);
+            Console.WriteLine("Shaked it.");
+            if (pattern.Length <= 1)
+                break;
+            pattern = pattern.Remove(length, 1);
+
+
+        }
+        Console.WriteLine("No shake.");
+        Console.WriteLine(text);
+    }
+}
